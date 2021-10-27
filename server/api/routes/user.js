95068c43aee44c81840f73f39f2d0625bdde4181
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2021-10-27 20:42:13 -0300

    endpoints hotfix

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -2,6 +2,14 @@ var express = require("express");
 var router = express.Router();
 var UserController = require("../../controllers/user");
 
+//CreateUser
+router.post("/", (req, res, next) => {
+  const { email, password, roles } = req.body;
+
+  UserController.createUser(email, password, roles);
+  res.status(200).send({ ok: true });
+});
+
 //GetById
 router.get("/:userId", (req, res, next) => {
   let userId = req.params.userId;
@@ -22,35 +30,22 @@ router.get("/:userId", (req, res, next) => {
 
 //GetAll
 router.get("/", (req, res, next) => {
-  let userId = req.params.userId;
-  const users = UserController.getAllUsers(userId);
+  const users = UserController.getAllUsers();
   res.status(200).send({ ok: true, users });
 });
 
-//DeleteById????
-router.delete("/:userId", (req, res, next) => {
-  let userId = req.params.userId;
-  UserController.deleteUser(userId);
-  res.status(200).send({ ok: true });
-});
-
 //Edit
-router.put("/:userId", (req, res, next) => {
+router.put("/edit/:userId", (req, res, next) => {
   let data = req.body;
 
   UserController.editUser(data);
   res.status(200).send({ ok: true });
 });
 
-//CreateUser
-
-router.post("/", (req, res, next) => {
-  let user = new User();
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.roles = req.body.roles;
-
-  UserController.createUser(user);
+//DeleteById????
+router.put("/delete/:userId", (req, res, next) => {
+  let userId = req.params.userId;
+  UserController.deleteUser(userId);
   res.status(200).send({ ok: true });
 });
 
